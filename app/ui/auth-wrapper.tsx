e9fd3b88e9b98d6639d5bfe3f2dd532a1b6109df
denis-mahei <denmahei@gmail.com>
2026-06-27 23:27:35 +0300

    feat: add initial auth page layout

--- app/ui/auth-wrapper.tsx
+++ app/ui/auth-wrapper.tsx
@@ -5,7 +5,11 @@ interface AuthWrapperProps {
 }
 
 const AuthWrapper = ({ children }: AuthWrapperProps) => {
-  return <div className="bg-secondary-bg">{children}</div>;
+  return (
+    <div className="bg-secondary-bg rounded-4xl lg:min-h-184 lg:w-1/2 md:w-full md:min-h-full">
+      {children}
+    </div>
+  );
 };
 
 export default AuthWrapper;
